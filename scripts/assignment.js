52def16ccd97b109667abf0022feaaf88e02834c
romeperotti2990 <rome.perotti2990@stu.mtec.edu>
2025-09-15 09:18:44 -0600

    added exercise 1

---
+++ scripts/assignment.js
@@ -0,0 +1,17 @@
+function alertMessage(message) {
+    alert(message)
+    console.log(message)
+}
+
+function getWelcomeMessage(name) {
+    alert(name)
+    console.log(name)
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function multiply(a, b) {
+    return a * b
+}
